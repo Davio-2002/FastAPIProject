cf7aae8cf7082cbe4b97f975d5fb8381a66a2ec6
Davio-2002 <khach.dav2002@gmail.com>
2024-12-29 22:07:06 +0400

    update application name in configuration

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./app.db"
-    APP_NAME: str = "FastAPI Project"
+    APP_NAME: str = "FastAPIProject"
     DEBUG: bool = True
 
     class Config:
